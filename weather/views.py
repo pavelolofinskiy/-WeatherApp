3772c98acf3c592698750a7bd0af553873905252
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-02-21 11:00:49 +0300

    add for loop to views.py with weather info, add changes to admin.py and index.html

--- weather/views.py
+++ weather/views.py
@@ -1,19 +1,28 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 def index(request):
     appid = '4c88cdaed527545929061dd19080c585'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    city = 'London'
-    res = requests.get(url.format(city)).json()
 
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]["icon"],
-    }
 
-    context = {'info': city_info}
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"],
+        }
+
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities}
 
     return render(request, 'index.html', context)
