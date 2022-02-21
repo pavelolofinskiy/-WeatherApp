ee6ad580db56c73f59d691038a29f576d61c04ba
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-02-21 10:12:12 +0300

    add changes to views.py and models.py

--- weather/views.py
+++ weather/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     appid = '4c88cdaed527545929061dd19080c585'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
     city = 'London'
-    res = requests.get(url.format(city))
-    print(res.text)
-    return render(request, 'index.html')
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'index.html', context)
