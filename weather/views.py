0df419d9677e558af2aadfa3b25e242f10ec3dbb
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-02-21 11:41:33 +0300

    made last changes for weather app, add forms.py file, add changes to views.py and index.html

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,17 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = '4c88cdaed527545929061dd19080c585'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
-
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -23,6 +27,6 @@ def index(request):
 
         all_cities.append(city_info)
 
-    context = {'all_info': all_cities}
+    context = {'all_info': all_cities, 'form': form}
 
     return render(request, 'index.html', context)
