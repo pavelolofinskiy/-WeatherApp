e14b41310efef11f27e5491e456b6b378455d929
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-02-21 09:39:28 +0300

    add requests module to views file with new variables

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,11 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
+    appid = '4c88cdaed527545929061dd19080c585'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city))
+    print(res.text)
     return render(request, 'index.html')
